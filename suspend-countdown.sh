6d45fc843acfd7e5e6ec4a648632c8a2d5a76a2a
okdev99 <okdev99@gmail.com>
2024-07-20 12:57:11 +0300

    Added integer regocnising test.

--- suspend-countdown.sh
+++ suspend-countdown.sh
@@ -2,19 +2,35 @@
 
 hibernate=false
 
-if [[ $1 == "-h" ]]; then
-	hibernate=true
-	countdown=$2
-elif [[ $1 == "" ]]; then
-	echo "Usage: $0 [-h] time"
-	echo
-	echo "  -h	hibernate instead of suspend"
-	echo
-	echo "If hibernating does not work, then the script will call to suspend."
-	exit 0
-else
-	countdown=$1
-fi
+case $1 in
+	"")
+		echo "Usage: $0 [-h] time"
+		echo
+		echo "  -h	hibernate instead of suspend"
+		echo
+		echo "If hibernating does not work, then the script will call to suspend."
+		exit 0
+		;;
+	"-h")
+		hibernate=true
+		case $2 in
+			*[!0-9]*)
+				echo "The second argument was not a positive integer."
+				exit 1
+				;;
+			*)
+				countdown=$2
+				;;
+		esac
+		;;
+	*[!0-9]*)
+		echo "The first argument was not a positive integer."
+		exit 1
+		;;
+	*)
+		countdown=$1
+		;;
+esac
 
 echo "--  --  --"
 
